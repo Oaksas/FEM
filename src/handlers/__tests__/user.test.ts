5e09e6fb4e7ba62bc0987c0838118c7d9881a740
=oaksas <=>
2023-10-25 12:03:16 +0300

    Add unit && integration test starters

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,6 +1,18 @@
 import { describe } from "node:test";
-
+import * as user from "../user";
 describe('user hander', () => {
-    it('should create a user', () => {
+    it('should create a new user', async () => {
+        const req = {
+            body: { username: 'test', password: 'test' }
+        }
+
+        const res = {
+            json({ token }) {
+                expect(token).toBeTruthy()
+            }
+        }
+
+        await user.createUser(req, res, () => { })
+
     })
 })
